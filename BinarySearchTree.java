65a03ade77643ae47ca0c5434d80d98b3e190cfb
jbredwards <143640517+JulianUSM@users.noreply.github.com>
2023-10-23 14:43:51 -0400

    fix ITreeIterator typo and syntax improvements

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,13 +1,18 @@
-public abstract class BinarySearchTree { 
-	protected Node root; protected int size; 
-	protected static class Node { 
-		int data; 
-		Node left; 
-		Node right; 
-		Node(int data) { 
-			this.data = data; 
-			left = null; 
-			right = null; } } 
-	public abstract boolean add(int data); 
-	public int getSize() { 
-		return size; } }
+public abstract class BinarySearchTree
+{
+	protected Node root;
+	protected int size;
+
+	public abstract boolean add(final int data);
+	public int getSize() { return size; }
+
+	protected static class Node
+	{
+		int data;
+		Node left, right;
+		Node(final int data) {
+			this.data = data;
+			left = null;
+			right = null; }
+	}
+}
