1bb91ae7090ea65709778cf292906f80da39610a
TheHylianLegend <39139509+TheHylianLegend@users.noreply.github.com>
2023-10-23 14:46:16 -0400

    Implemented myBinarySearchTree

---
+++ myBinarySearchTree.java
@@ -0,0 +1,54 @@
+public class myBinarySearchTree extends BinarySearchTree{
+    protected Node root;
+    protected int size;
+    protected static class Node { 
+        int data; 
+        Node left; 
+        Node right; 
+        Node(int data) { 
+            this.data = data; 
+            left = null; 
+            right = null;
+            } 
+        } 
+    
+    public myBinarySearchTree() {
+        root = null;
+        size = 0;
+    }
+    
+    
+    public boolean add(int data) {
+        if (root == null) {
+            root = new Node(data);
+            size++;
+            return true;
+        }
+        
+        Node localRoot = root;
+        for (;;) {
+            if (data < localRoot.data) {
+                if (localRoot.left != null)
+                    localRoot = localRoot.left;
+                else {
+                    size++;
+                    localRoot.left = new Node(data);
+                    return true;
+                }    
+            }
+            if (data >= localRoot.data) {
+                if (localRoot.right != null)
+                    localRoot = localRoot.right;
+                else {
+                    size++;
+                    localRoot.right = new Node(data);
+                    return true;
+                }
+            }
+        }
+    }
+    
+    public int getSize() {
+        return size;
+    }
+}
